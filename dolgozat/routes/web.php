15541906e3924f0c5724ec95cc452f1f5536b398
Molnár Levent <mollev545@hengersor.hu>
2024-09-23 10:27:43 +0200

    kesz

--- dolgozat/routes/web.php
+++ dolgozat/routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/bejeltkezes', function () {
+Route::get('/bejelentkezes', function () {
     return view('bejelentkezes');
 });
 Route::get('/bemutatkozas', function () {
