28a971e40a8d0ab75d411ba8b622dacac5185439
Molnár Levent <mollev545@hengersor.hu>
2024-09-23 10:23:13 +0200

    elso

--- dolgozat/routes/web.php
+++ dolgozat/routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/bejeltkezes', function () {
+    return view('bejelentkezes');
+});
+Route::get('/bemutatkozas', function () {
+    return view('bemutatkozas');
+});
+Route::get('/felhasznalo', function () {
+    return view('felhasznalo');
+});
+
+
